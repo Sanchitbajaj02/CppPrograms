07fbb8b9b0f6aee9ef794abeea85618b6fa514cf
tusharjuneja06 <72194746+tusharjuneja06@users.noreply.github.com>
2020-10-01 17:20:40 +0530

    Create pattren.cpp

---
+++ pattren.cpp
@@ -0,0 +1,12 @@
+#inculde <iostream>
+using namespace std;
+int main(){
+
+
+for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << i;
+        }
+    }
+    return 0;
+    }
