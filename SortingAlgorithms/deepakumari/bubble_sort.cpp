68a2ae4996e926ece2ee3440aa95d00977ba13a8
deepakuma <deepakumari9999999@gmail.com>
2020-10-05 20:09:59 +0530

    Added BubbleSort

---
+++ SortingAlgorithms/deepakumari/bubble_sort.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void bubble_sort(int *arr,int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+void print(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bubble_sort(arr,n);
+    print(arr,n);
+    return 0;
+}
