768036f77c9efa3f5400b7670d2c22434ab7a94e
Atharva Parikh <54139853+Atharva-Parikh@users.noreply.github.com>
2020-10-20 19:14:31 +0530

    Updated Linear search.
    made it user friendly :)

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,11 +1,17 @@
 
-// C++ code to linearly search x in arr[]. If x
-// is present then return its location, otherwise
-// return -1
-
+/* C++ code to linearly search x in arr[]. If x is present then return its location, otherwise return -1.
+if elements are same then it will return first one in the sequence.
+*/
 #include <iostream>
 using namespace std;
-
+void accept(int arr[],int n)
+{
+	int i;
+	for(i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+}
 int search(int arr[], int n, int x)
 {
     int i;
@@ -17,9 +23,18 @@ int search(int arr[], int n, int x)
 
 int main(void)
 {
-    int arr[] = { 8, 4, 5, 10, 60 };
-    int x = 10;
-    int n = sizeof(arr) / sizeof(arr[0]);
+	int n,x; 
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter "<<n<<" elements seperated by space"<<endl;
+    accept(arr,n);
+    /*for(int i=0;i<n;i++)
+    {
+    	cin>>arr[i];
+	}*/
+    cout<<"Enter the element to be searched: ";
+    cin>>x;
     int result = search(arr, n, x);
    (result == -1)? cout<<"Element is not present in array"
                  : cout<<"Element is present at index " <<result;
